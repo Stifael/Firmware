730914400bb6599ec6206c5a1e01e5540a31c0b8
Martina <martina@rivizzigno.it>
2017-08-31 15:44:26 +0200

    rtl: add geofence action and altitude parameters

--- src/modules/navigator/rtl.h
+++ src/modules/navigator/rtl.h
@@ -48,6 +48,7 @@
 #include <navigator/navigation.h>
 #include <uORB/topics/home_position.h>
 #include <uORB/topics/vehicle_global_position.h>
+#include <uORB/topics/geofence_result.h>
 
 #include "navigator_mode.h"
 #include "mission_block.h"
@@ -98,6 +99,8 @@ private:
 	control::BlockParamFloat _param_descend_alt;
 	control::BlockParamFloat _param_land_delay;
 	control::BlockParamFloat _param_rtl_min_dist;
+	control::BlockParamFloat _param_gf_alt;
+	control::BlockParamInt 	 _param_gf_actions;
 };
 
 #endif
